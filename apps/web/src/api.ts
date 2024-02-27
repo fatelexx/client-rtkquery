f2c9d0b7bd30294eb280529fc9942708b28f5529
fatelexx <fatelexx@outlook.com>
2024-02-27 19:50:56 +0800

    chore: setup api and provider

---
+++ apps/web/src/api.ts
@@ -0,0 +1,39 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { CatDto } from "@client-rtkquery/dto";
+
+export const catApi = createApi({
+    reducerPath: 'cat',
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/api/cats' }),
+    endpoints: (builder) => ({
+      getAll: builder.query<Array<CatDto>, void>({
+        query: () => ``,
+      }),
+      get: builder.query<Array<CatDto>, number>({
+        query: (id) => `${id}`,
+      }),
+      create: builder.mutation<number, CatDto>({
+        query: (cat) => ({
+          url: ``,
+          body: cat,
+          method: 'POST'
+        }),
+        
+      }),
+      update: builder.mutation<void, CatDto>({
+        query: (cat) => ({
+          url: `${cat.id}`,
+          body: cat,
+          method: 'PUT'
+        })
+      }),
+      delete: builder.mutation<void, number>({
+        query: (message) => ({
+          url: `/${message}`,
+          method: 'DELETE'
+        }),
+        
+      })
+    }),
+  })
+
+  export const { useGetAllQuery, useCreateMutation, useDeleteMutation, useGetQuery, useUpdateMutation } = catApi;
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,13 +1,21 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import {
+  ApiProvider,
+} from '@reduxjs/toolkit/query/react'
 
-import App from './app/app';
+import { catApi } from './api';
+import App from './app';
+import Form from './form';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <App />
+    <ApiProvider api={catApi}>
+      <App />
+      <Form />
+    </ApiProvider>
   </StrictMode>
 );
