966197361306ef684f2e1171c29228d56b2796fe
fatelexx <fatelexx@outlook.com>
2024-02-28 17:40:00 +0800

    chore: auto refresh thru cache tags

--- apps/web/src/api.ts
+++ apps/web/src/api.ts
@@ -4,12 +4,15 @@ import { CatDto } from "@client-rtkquery/dto";
 export const catApi = createApi({
     reducerPath: 'cat',
     baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/api/cats' }),
+    tagTypes: ['Cats'],
     endpoints: (builder) => ({
       getAll: builder.query<Array<CatDto>, void>({
         query: () => ``,
+        providesTags: ['Cats'],
       }),
       get: builder.query<CatDto, number>({
         query: (id) => `${id}`,
+        providesTags: ['Cats'],
       }),
       create: builder.mutation<number, CatDto>({
         query: (cat) => ({
@@ -17,21 +20,22 @@ export const catApi = createApi({
           body: cat,
           method: 'POST'
         }),
-        
+        invalidatesTags: ['Cats']
       }),
       update: builder.mutation<void, CatDto>({
         query: (cat) => ({
           url: `${cat.id}`,
           body: cat,
           method: 'PUT'
-        })
+        }),
+        invalidatesTags: ['Cats']
       }),
       delete: builder.mutation<void, number>({
         query: (message) => ({
           url: `/${message}`,
           method: 'DELETE'
         }),
-        
+        invalidatesTags: ['Cats']
       })
     }),
   })
