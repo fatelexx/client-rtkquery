f329f5a18b8de620fc7443e5f9277e1fc0ba69b8
fatelexx <fatelexx@outlook.com>
2024-03-13 23:01:58 +0800

    chore: migrate cats schema to db

---
+++ apps/backend/prisma/migrations/20240313150122_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Cat" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT,
+    "birthDay" DATETIME NOT NULL
+);
